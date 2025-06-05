7a97e2651fb526e3bd47f8898721493af7966eb0
caio-pavesi <ocaiopavesi@hotmail.com>
2025-06-04 22:53:19 -0300

    feat: Database tables scheme

---
+++ sql/tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS jobs (
+    id INTEGER PRIMARY KEY,
+    job_portal_id INTEGER,
+    job_link TEXT,
+    job_page_source TEXT,
+    job_title TEXT,
+    job_description TEXT,
+    job_type TEXT,
+    job_field TEXT,
+    job_city TEXT
+);
+
+CREATE TABLE IF NOT EXISTS matches (
+    id INTEGER PRIMARY KEY,
+    job_portal_id INTEGER,
+    score FLOAT,
+    strengths TEXT,
+    weaknesses TEXT,
+    improvement TEXT,
+    conclusion TEXT,
+    interview TEXT,
+    FOREIGN KEY (job_portal_id) REFERENCES jobs(job_portal_id)
+);
