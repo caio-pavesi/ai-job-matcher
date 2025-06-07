a230351fdb59f992321ebf0273e50c245c842da5
caio-pavesi <ocaiopavesi@hotmail.com>
2025-06-07 15:44:47 -0300

    feat: Instead of creating only a query, created a table from the join of the others, easier to query and gather info

--- sql/tables.sql
+++ sql/tables.sql
@@ -21,3 +21,21 @@ CREATE TABLE IF NOT EXISTS matches (
     match_possible_interview BOOLEAN,
     FOREIGN KEY (job_portal_id) REFERENCES jobs(job_portal_id)
 );
+
+CREATE TABLE IF NOT EXISTS job_matches AS SELECT
+    jobs.job_portal_id,
+    jobs.job_link,
+    jobs.job_title,
+    jobs.job_description,
+    jobs.job_posting_date,
+    jobs.job_type,
+    jobs.job_field,
+    jobs.job_city,
+    matches.match_score,
+    matches.match_strengths,
+    matches.match_weaknesses,
+    matches.match_improvement_points,
+    matches.match_conclusions,
+    matches.match_possible_interview
+FROM matches
+LEFT JOIN jobs ON jobs.job_portal_id = matches.job_portal_id;
