0be6478d6f514dec5090c05ff09c5e46d6c66e42
caio-pavesi <ocaiopavesi@hotmail.com>
2025-08-30 22:32:32 -0300

    fix: Created a more realistic filter query

--- sql/filter.sql
+++ sql/filter.sql
@@ -1,3 +1,5 @@
-SELECT job_portal_id, job_description FROM jobs
-WHERE job_portal_id NOT IN
-(SELECT job_portal_id FROM matches);
+SELECT * FROM jobs WHERE
+1 = 1
+AND job_title LIKE '%international%intern%'
+OR job_title LIKE '%trainee%'
+AND job_portal_id NOT IN (SELECT job_portal_id FROM matches);
